837efc94272b6a940e7808999c809301f4aa4786
Eric Loots <eric.loots@gmail.com>
2018-04-03 11:19:56 +0200

    Insert a new exercise that will switch remote transport to Artery/TCP
    This commit just copies the base cluster example into a new exercise.
    The actual switching of the remote transport is done in the next
    commit. The README file for the new exercise is already updated though.
    
    Starting for the base Akka Cluster example, the Akka remote transport
    will be switched from the default one to Artery TCP (introduced in Akka
    2.5.11)

---
+++ exercise_005_cluster_base_move_to_artery_tcp/src/main/scala/org/neopixel/ClusterStatusTracker.scala
@@ -0,0 +1,146 @@
+/**
+  * Copyright © 2018 Lightbend, Inc
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  *
+  * NO COMMERCIAL SUPPORT OR ANY OTHER FORM OF SUPPORT IS OFFERED ON
+  * THIS SOFTWARE BY LIGHTBEND, Inc.
+  *
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+package org.neopixel
+
+import akka.actor.{Actor, ActorLogging, Props, Timers}
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent._
+import akka.cluster.MemberStatus.{Up, WeaklyUp}
+
+object ClusterStatusTracker {
+
+  def resetAllLeds(strip: Adafruit_NeoPixel.type): Unit = {
+    for {
+      pixel <- 0 until strip.numPixels()
+    } strip.setPixelColor(pixel, Black)
+    strip.show()
+  }
+
+  case object Heartbeat
+
+  def props(strip: Adafruit_NeoPixel.type): Props = Props(new ClusterStatusTracker(strip))
+}
+
+class ClusterStatusTracker(strip: Adafruit_NeoPixel.type) extends Actor with ActorLogging with SettingsActor with Timers {
+  import ClusterStatusTracker._
+
+  private val thisHost = context.system.settings.config.getString("akka.remote.netty.tcp.hostname")
+  log.debug(s"Starting ClusterStatus Actor on $thisHost")
+
+  import settings.LedStripConfig._
+  import settings._
+
+  private val LeaderLedNumber = highestLedIndex - 5
+  private val HeartbeatLedNumber = highestLedIndex - 7
+
+  override def receive: Receive = idle
+
+  def idle: Receive = akka.actor.Actor.emptyBehavior
+
+  def running(hearbeatLEDOn: Boolean): Receive = {
+    case Heartbeat if hearbeatLEDOn =>
+      setPixelColorAndShow(strip, HeartbeatLedNumber, Black)
+      context.become(running(hearbeatLEDOn = false))
+
+    case Heartbeat =>
+      setPixelColorAndShow(strip, HeartbeatLedNumber, heartbeartIndicatorColor)
+      context.become(running(hearbeatLEDOn = true))
+
+    case msg @ MemberUp(member) =>
+      setPixelColorAndShow(strip, HostToLedMapping(member.address.host.get), nodeUpColor)
+      log.debug(s"$msg")
+
+    case msg @ MemberLeft(member) =>
+      setPixelColorAndShow(strip, HostToLedMapping(member.address.host.get), nodeLeftColor)
+      log.debug(s"$msg")
+
+    case msg @ MemberExited(member) =>
+      setPixelColorAndShow(strip, HostToLedMapping(member.address.host.get), nodeExitedColor)
+      log.debug(s"$msg")
+
+    case msg @ MemberJoined(member) =>
+      setPixelColorAndShow(strip, HostToLedMapping(member.address.host.get), nodeJoinedColor)
+      log.debug(s"$msg")
+
+    case msg @ MemberRemoved(member, previousStatus) =>
+      setPixelColorAndShow(strip, HostToLedMapping(member.address.host.get), nodeDownColor)
+      log.debug(s"$msg")
+
+    case msg @ MemberWeaklyUp(member) =>
+      setPixelColorAndShow(strip, HostToLedMapping(member.address.host.get), nodeWeaklyUpColor)
+      log.debug(s"$msg")
+
+    case msg @ ReachableMember(member) if member.status == Up =>
+      setPixelColorAndShow(strip, HostToLedMapping(member.address.host.get), nodeUpColor)
+      log.debug(s"$msg")
+
+    case msg @ ReachableMember(member) if member.status == WeaklyUp =>
+      setPixelColorAndShow(strip, HostToLedMapping(member.address.host.get), nodeWeaklyUpColor)
+      log.debug(s"$msg")
+
+    case msg @ UnreachableMember(member) =>
+      setPixelColorAndShow(strip, HostToLedMapping(member.address.host.get), nodeUnreachableColor)
+      log.debug(s"$msg")
+
+    case msg @ LeaderChanged(Some(leader)) if leader.host.getOrElse("") == thisHost =>
+      setPixelColorAndShow(strip, LeaderLedNumber, leaderIndicatorColor)
+      log.debug(s"$msg")
+
+    case msg @ LeaderChanged(Some(leader)) =>
+      setPixelColorAndShow(strip, LeaderLedNumber, Black)
+      log.debug(s"$msg")
+
+    case msg @ LeaderChanged(None) =>
+      setPixelColorAndShow(strip, LeaderLedNumber, Black)
+      log.debug(s"$msg")
+
+    case event =>
+      log.debug(s"~~~> UNHANDLED CLUSTER DOMAIN EVENT: $event")
+
+  }
+
+  override def preStart(): Unit = {
+    strip.begin()
+    resetAllLeds(strip)
+    Cluster(context.system)
+      .subscribe(self,
+        InitialStateAsEvents,
+        classOf[LeaderChanged],
+        classOf[ReachabilityEvent],
+        classOf[MemberEvent]
+      )
+    timers.startPeriodicTimer("heartbeat-timer", Heartbeat, heartbeatIndicatorInterval)
+    context.become(running(hearbeatLEDOn = false))
+
+  }
+
+  override def postStop(): Unit = {
+    resetAllLeds(strip)
+    Cluster(context.system).unsubscribe(self)
+  }
+
+  private def setPixelColorAndShow(strip: Adafruit_NeoPixel.type ,
+                                   ledNumber: Int,
+                                   ledColor: Long): Unit = {
+    strip.setPixelColor(ledNumber, ledColor)
+    strip.show()
+  }
+}
